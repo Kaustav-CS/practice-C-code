e1fde430afe8b27ec90c7750d5e0ac0b1fac912a
Kaustav-CS <kaustav.pg2019@ee.iiests.ac.in>
2020-12-31 22:01:56 +0530

    new line added 10

--- matlab_01.c
+++ matlab_01.c
@@ -7,3 +7,4 @@
 clc;close all;clear all;format short
 a=5;b=15;c=-(b-a);
 R=randi(9,2)*c
+// new line 10;
---
+++ matlab_02.m
@@ -0,0 +1,7 @@
+
+
+%matlab test 2020-12-31; 21:58
+
+clc;close all;clear all;format short
+a=5;b=15;c=-(b-a);
+R=randi(9,2)*c
