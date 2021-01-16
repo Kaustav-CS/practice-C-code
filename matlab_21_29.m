525bebe34f674d4c79613295acb06a06feed948b
Kaustav-CS <kaustav.pg2019@ee.iiests.ac.in>
2021-01-16 21:30:21 +0530

    Demostration of surface plot; matlab_21_29.m

---
+++ matlab_21_29.m
@@ -0,0 +1,16 @@
+[x,y] = peaks(30);
+z = exp(-0.9*(x.^2+0.5*(x-y).^2));
+surf(x,y,z);
+xlabel('\bf X axis');
+ylabel('\bf Y axis');
+zlabel('\bf Z axis');
+title('\bf Surface Plot')
+colorbar
+Location=pwd
+Date=date
+
+% Result
+% Location =
+%     '/MATLAB Drive/Prac 01'
+% Date =
+%     '16-Jan-2021'
